1c5e92fc806c0e83c6061dec5ae9ff1e44ceaee4
Vishal-alpha3 <vishalvishwa0412@gmail.com>
2024-03-09 15:28:24 +0530

    change in css and app.js

--- app.js
+++ app.js
@@ -23,6 +23,30 @@
       return(
         <div className="header">
 
+          <div className="logo-container">
+              <img className="logo"src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          </div>
+
+          
+
+              <ul className="navbar">
+                  <li className="navbar-item">
+                    <a className="navbar-link">Home</a>
+                  </li>
+                  <li className="navbar-item">
+                    <a className="navbar-link">About Us</a>
+                  </li>
+                  <li className="navbar-item">
+                    <a className="navbar-link">Contact Us</a>
+                  </li>
+                  <li className="navbar-item">
+                    <a className="navbar-link">Cart</a>
+                  </li>
+                     
+              </ul>
+
+          
+
         </div>
       )
     }
@@ -30,7 +54,7 @@
     const AppComponent = () => {
       return(
         <div className="app">
-           
+           <Header/>
         </div>
       )
     }
