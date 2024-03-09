5b9921683d2480142ff034df012ca7d83f6ec8f9
Vishal-alpha3 <vishalvishwa0412@gmail.com>
2024-03-09 14:16:13 +0530

    in link we did change in header section in link element of style.css

--- app.js
+++ app.js
@@ -2,32 +2,41 @@
     import React from "react";
     import ReactDOM from "react-dom/client";
 
-//
-      const title  =  (
-          <h1 className="head" tabIndex="5">
-            This is the one more component.
-            </h1>
-            );
+    /**
+     * header
+     *   -logo
+     *   -navbar
+     *   -cart
+     * body
+     *   -searchbar
+     *   -restorant container
+     *      -restorant carts
+     * footer
+     *   -copyright
+     *   -links
+     *   -address
+     *   -contact
+     */
 
 
+    const Header = () => {
+      return(
+        <div className="header">
 
-//this react component
-
-      const HeadingComponent = ()=> (
-        <div className="constainer"> 
-
-            {title}
-          <h1>
-            Namastey React function Componenet
-          </h1>
         </div>
-      );
-        
-       
-      
+      )
+    }
+
+    const AppComponent = () => {
+      return(
+        <div className="app">
+           
+        </div>
+      )
+    }
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(<HeadingComponent/>);
+    root.render(<AppComponent/>);
 
   
